7aaeac2bb269e5a2d69d59eb9f20dd916c0f0798
Luiza Souza Chaves Lessa Campos <luizasclcampos@gmail.com>
2026-09-12 17:49:49 +0000

    lab 7 - térmico

--- inf1018/lab7/ex3.c
+++ inf1018/lab7/ex3.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 
+int main(void) {
+    int i; //variável local p iterar de 1 a 10
+
+    for (i = 1; i <= 10; i++) {
+        //calcula o quadrado multiplicando a variável por ela mesma e imprime
+        printf("O quadrado de %d eh: %d\n", i, i * i);
+    }
+
+    return 0;
+}
+
